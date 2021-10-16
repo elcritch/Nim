80315d44121a388dc49a60f2c1e13412eaf0b307
Jaremy J. Creechley <jaremy.creechley@wavebaselabs.com>
2021-10-15 19:13:16 -0700

    Porting Nim to run on Zephyr.
    Includes changes to `std/net`.
    
    Squashed commit of the following:
        tweaking more memory / malloc things
        revert back bitmasks
        tweaking nim to use kernel heap as C malloc doesn't work
        fixing socket polling on zephyr
        cleanup getting maximum sockets for process or for rtos'es
        reorganizing and fixing net for async / system
        merge netlite changes back into nativesockets
        merge netlite changes back into nativesockets
        reverting native sockets back
        tweaking nim / zephyr network
        adding option to run 'net-lite' from linux
        bridging zephyr's max connections
        fixing net errors
        fixing compilation with getAddrString
        fixing compilation with getAddrString
        experimenting with a nativesockets_lite ... getAddrString
        experimenting with a nativesockets_lite ... getAddrString
        experimenting with a nativesockets_lite ... getLocalAddr
        experimenting with a nativesockets_lite ... getLocalAddr
        experimenting with a nativesockets_lite ...
        add note regarding incorrect FreeRTOS Sockadd_in fields
        changing to NIM_STATIC_ASSERT
        cleaning up the static_assert error messages
        cleaning up the static_assert error messages
        setting up static assert ftw!
        testing compile time asserts
        reworking Sockaddr objects to more closely match various platforms
        reworking Sockaddr objects to more closely match various platforms
        reworking Sockaddr objects to more closely match various platforms
        finding missing items (issue  #18684)
        fixup posix constants (issue  #18684)
        adding plumbing for zephyr os (issue  #18684)
        adding plumbing for zephyr os (issue  #18684)

--- compiler/options.nim
+++ compiler/options.nim
@@ -575,7 +575,7 @@ proc isDefined*(conf: ConfigRef; symbol: string): bool =
                             osQnx, osAtari, osAix,
                             osHaiku, osVxWorks, osSolaris, osNetbsd,
                             osFreebsd, osOpenbsd, osDragonfly, osMacosx, osIos,
-                            osAndroid, osNintendoSwitch, osFreeRTOS}
+                            osAndroid, osNintendoSwitch, osFreeRTOS, osZephyr}
     of "linux":
       result = conf.target.targetOS in {osLinux, osAndroid}
     of "bsd":
@@ -593,6 +593,8 @@ proc isDefined*(conf: ConfigRef; symbol: string): bool =
       result = conf.target.targetOS == osNintendoSwitch
     of "freertos", "lwip":
       result = conf.target.targetOS == osFreeRTOS
+    of "zephyr":
+      result = conf.target.targetOS == osZephyr
     of "littleendian": result = CPU[conf.target.targetCPU].endian == littleEndian
     of "bigendian": result = CPU[conf.target.targetCPU].endian == bigEndian
     of "cpu8": result = CPU[conf.target.targetCPU].bit == 8
